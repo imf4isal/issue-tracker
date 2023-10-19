4b90e9d6fe54c62a971204508475483d2b1dc892
Faisal <md.faisal1899@yahoo.com>
2023-10-19 21:37:01 +0600

    add Skeleton component

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,5 @@
+import Skeleton from '@/app/components/Skeleton';
 import { Table } from '@radix-ui/themes';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 import NewIssueAction from '../components/NewIssueAction';
 
 const loading = () => {
