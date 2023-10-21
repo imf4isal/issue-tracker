34d16a011baa2a4a9b3007a094acdc19d6e1739d
Faisal <md.faisal1899@yahoo.com>
2023-10-21 12:36:58 +0600

    troubleshoot for rendering user avatar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // trouble shoot for user data
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/:path*',
+    //             headers: [{ key: 'referrer-policy', value: 'no-referrer' }],
+    //         },
+    //     ];
+    // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
